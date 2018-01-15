5e3d3ece6c286938537332a1a44c544fb4786671
Mario Lang <mlang@delysid.org>
2018-01-15 19:38:59 +0100

    add an info.rkt to hopefully fix checksum error with pkgs.racket-lang.org

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "mario")
+(define version "0.01")
+(define deps '())
+(define pkg-desc "An interpreter for the 2d programming language MarioLANG")
+
+(define build-deps '())
+(define scribblings '())
